f8f69cd44e9d95856dc8cb2dce59e1ac6a4bc437
crohnarodev <viniciosalfa@hotmail.com>
2023-08-25 21:23:36 -0300

    feat: :sparkles: adding age and gender to the users model

--- src/models/users.js
+++ src/models/users.js
@@ -13,6 +13,15 @@ const schema = new mongoose.Schema({
     type: String,
     required: [true, 'O campo "senha" é obrigatório.'],
   },
+  idade: {
+    type: Number,
+    required: [true, 'O campo "idade" é obrigatório.'],
+  },
+  genero: {
+    type: String,
+    required: [true, 'O campo "gênero" é obrigatório.'],
+  },
+  
 });
 
 export default mongoose.models.users || mongoose.model("users", schema);
