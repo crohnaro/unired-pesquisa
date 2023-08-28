186622afbfe7832cc5bf8956e403ced012b1d2da
crohnaro <vinicioscararine95@gmail.com>
2023-08-28 12:37:50 -0300

    feat: :sparkles: changing the dependencies and the structure of all the project

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,25 @@
-import '@/styles/globals.css'
+import * as React from "react";
+import { ToastyProvider } from "../src/contexts/Toasty";
+import CssBaseline from "@mui/material/CssBaseline";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { SessionProvider } from "next-auth/react";
+import CheckAuth from "../src/components/CheckAuth";
+
+
+export default function MyApp({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
+  return (
+    <SessionProvider session={pageProps.session}>
+      <ToastyProvider>
+        <CssBaseline />
+        {Component.requireAuth ? (
+          <CheckAuth Component={Component} pageProps={pageProps} />
+        ) : (
+          <Component {...pageProps} />
+        )}
+      </ToastyProvider>
+    </SessionProvider>
+  );
 }
