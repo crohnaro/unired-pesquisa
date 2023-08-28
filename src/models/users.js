e6c2bae2a29d708f06611d905d4e6d3f6b1b76f0
crohnaro <crohnarodev@gmail.com>
2023-08-28 12:41:19 -0300

    refactor: :recycle: changing the dependencies and structure of the project

---
+++ src/models/users.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'O campo "nome" é obrigatório.'],
+  },
+  email: {
+    type: String,
+    required: [true, 'O campo "e-mail" é obrigatório.'],
+  },
+  password: {
+    type: String,
+    required: [true, 'O campo "senha" é obrigatório.'],
+  },
+  idade: {
+    type: Number,
+    required: [true, 'O campo "idade" é obrigatório.'],
+  },
+});
+
+export default mongoose.models.users || mongoose.model("users", schema);
---
+++ src/utils/password.js
@@ -0,0 +1,17 @@
+import bcrypt from "bcrypt";
+
+const crypto = async (pwd) => {
+  const salt = await bcrypt.genSalt();
+
+  const password = await bcrypt.hash(pwd, salt);
+
+  return password;
+};
+
+const compare =  (pwd, hash) => {
+  const result = bcrypt.compareSync(pwd, hash)
+
+  return result
+}
+
+export { crypto, compare, };
