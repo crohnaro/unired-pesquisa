36f76310d5bc21cc1c6ddee3995ce9738ad51aa5
crohnaro <vinicioscararine95@gmail.com>
2023-08-29 10:37:58 -0300

    fix: :bug: fixing mismatch initial values

--- src/lib/formValueSignup.js
+++ src/lib/formValueSignup.js
@@ -1,16 +1,17 @@
 import * as yup from "yup";
 
 const initialValues = {
+  name: "",
   email: "",
   password: "",
   passwordConf: "",
-  genero: "",
   idade: "",
   gender: "",
   course: "",
 };
 
 const validationSchema = yup.object().shape({
+  name: yup.string().required("Campo obrigatório"),  
   email: yup
     .string()
     .email("Digite um e-mail válido")
