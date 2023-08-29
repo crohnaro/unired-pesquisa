df980ed122d0fa1ff4cadf4dd6da6184539c471c
crohnarodev <viniciosalfa@hotmail.com>
2023-08-28 21:06:22 -0300

    fix: :bug: fixing a bug where session token causes an error when rendering the page

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,36 @@
-import { Inter } from 'next/font/google'
+import { Inter } from "next/font/google";
 
 import { getSession, signOut } from "next-auth/react";
 import dbConnect from "../src/utils/dbConnect";
-import { Button } from '@mui/material';
+import { Button } from "@mui/material";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 const Home = () => {
   return (
     <>
       <h1>Home</h1>
-      <Button onClick={() => {
-        signOut({
-          callbackUrl: "/auth/signin"
-        })
-      }}>Sair</Button>
+      <Button
+        onClick={() => {
+          signOut({
+            callbackUrl: "/auth/signin",
+          });
+        }}
+      >
+        Sair
+      </Button>
       <h1>Teste</h1>
     </>
-  )
-}
+  );
+};
 
-Home.requireAuth = true
+Home.requireAuth = true;
 
 export async function getServerSideProps({ req }) {
   const session = await getSession({ req });
   await dbConnect();
 
-  let token = "";
-  session.accessToken
-    ? (token = session.accessToken)
-    : (token = session.user.email);
+  let token = session?.accessToken || session?.user?.email || "";
 
   console.log(session);
   console.log(token);
@@ -39,7 +40,6 @@ export async function getServerSideProps({ req }) {
       session: JSON.parse(JSON.stringify(session)),
     }
   }
-
 }
 
 export default Home;
