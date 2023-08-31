6356818721a1fd3701a1ea0df0d8379b04689ee5
crohnarodev <viniciosalfa@hotmail.com>
2023-08-31 18:55:49 -0300

    fix: :fire: removing theme provider that causes crash on the application

--- pages/_app.js
+++ pages/_app.js
@@ -4,27 +4,21 @@ import CssBaseline from "@mui/material/CssBaseline";
 
 import { SessionProvider } from "next-auth/react";
 import CheckAuth from "../src/components/CheckAuth";
-import theme from "@/src/theme/theme";
-import { ThemeProvider } from "@mui/joy";
-
 
 export default function MyApp({
   Component,
   pageProps: { session, ...pageProps },
 }) {
   return (
-    <ThemeProvider theme={theme}>
-      <SessionProvider session={pageProps.session}>
+    <SessionProvider session={pageProps.session}>
       <ToastyProvider>
         <CssBaseline />
         {Component.requireAuth ? (
-          <CheckAuth  Component={Component} pageProps={pageProps} />
+          <CheckAuth Component={Component} pageProps={pageProps} />
         ) : (
           <Component {...pageProps} />
         )}
       </ToastyProvider>
     </SessionProvider>
-    </ThemeProvider>
-    
   );
 }
