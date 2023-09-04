63e10ca986790a1c682bf679ea861ab26a7823b0
crohnaro <vinicioscararine95@gmail.com>
2023-09-04 10:57:56 -0300

    feat: :sparkles: adding questions models

---
+++ src/models/questions.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema({
+  text: {
+    type: String,
+    required: true,
+    // O texto da pergunta
+  },
+});
+
+export default mongoose.models.questions || mongoose.model("questions", schema);
