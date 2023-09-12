f92e657aea1da7e3599ca12c5d2157d8d58a3bbf
crohnarodev <viniciosalfa@hotmail.com>
2023-09-12 18:48:24 -0300

    fix: :construction: adding user data from getserversideprops

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,6 @@ import useToasty from "../src/contexts/Toasty";
 
 import theme from "@/src/theme/JoyTheme/theme";
 
-
 import {
   Box,
   Button,
@@ -19,13 +18,13 @@ import {
   FormControl,
   FormHelperText,
   Select,
-  Typography,
   Input,
   MenuItem,
   CircularProgress,
   FormLabel,
-  CssBaseline,
 } from "@mui/material";
+
+import Typography from "@mui/joy/Typography";
 
 import { getSession } from "next-auth/react";
 import dbConnect from "../src/utils/dbConnect";
@@ -33,14 +32,16 @@ import UsersModel from "../src/models/users";
 
 import TemplateDefault from "../src/template/Default";
 import { Formik } from "formik";
-import { initialValues, validationSchema } from "@/src/lib/formValueAnswersForms";
+import {
+  initialValues,
+  validationSchema,
+} from "@/src/lib/formValueAnswersForms";
 
-
-
-const Home = ({ userEmail }) => {
+const Home = ({ user }) => {
   const router = useRouter();
   const { setToasty } = useToasty();
 
+  console.log(user);
 
   const handleFormSubmit = async (formJson) => {
     const response = await axios.post("/api/answersforms", formJson);
@@ -62,193 +63,141 @@ const Home = ({ userEmail }) => {
   const formValues = {
     ...initialValues,
   };
-  formValues.userEmail = userEmail;
 
   return (
     <TemplateDefault>
-      <CssBaseline />
-      <Box
-        sx={(theme) => ({
-          width: "100%",
-          transition: "width var(--Transition-duration)",
-          transitionDelay: "calc(var(--Transition-duration) + 0.1s)",
-          position: "relative",
-          zIndex: 1,
-          display: "flex",
-          justifyContent: "center",
-          backdropFilter: "blur(4px)",
-          backgroundColor: "rgba(255 255 255 / 0.6)",
-          [theme.getColorSchemeSelector("dark")]: {
-            backgroundColor: "rgba(19 19 24 / 0.4)",
-          },
-        })}
-      >
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            minHeight: "100dvh",
-            width:
-              "clamp(var(--Form-maxWidth), (var(--Collapsed-breakpoint) - 100vw) * 999, 100%)",
-            maxWidth: "100%",
-            px: 2,
-          }}
+      <Container maxWidth="md" sx={{marginTop: 10}}>
+        <div>
+          <Typography component="h1" fontSize="xl2" fontWeight="lg">
+            Prezado(a) Estudante
+          </Typography>
+          <Typography level="body-sm" sx={{ my: 1, mb: 3 }}>
+            É com grande interesse que convidamos você a participar de um
+            momento fundamental para a pesquisa que está sendo conduzida no
+            âmbito da UniRedentor. Esta pesquisa tem como objetivo aprofundar
+            nossa compreensão sobre as percepções e sentimentos dos estudantes
+            em relação à sua experiência acadêmica, especificamente no que diz
+            respeito à graduação que estão cursando.
+          </Typography>
+          <Typography level="body-sm" sx={{ my: 1, mb: 3 }}>
+            Sabemos que a vida acadêmica é repleta de desafios e realizações, e
+            a sua perspectiva é inestimável para enriquecer nosso entendimento
+            sobre os diversos aspectos envolvidos. Sua participação ao responder
+            o questionário contribuirá significativamente para o avanço do
+            conhecimento na área de Ensino das Ciências e Saúde, possibilitando
+            a criação de estratégias mais eficazes para melhorar a qualidade da
+            educação e o bem-estar dos estudantes.
+          </Typography>
+          <Typography level="body-sm" sx={{ my: 1, mb: 3 }}>
+            Este SCORE busca capturar seus sentimentos e percepções genuínos em
+            relação à sua graduação. Queremos que você se sinta à vontade para
+            expressar seus pensamentos de maneira verdadeira e autêntica. Suas
+            respostas serão tratadas com total confidencialidade e serão
+            utilizadas apenas para fins de análise estatística e pesquisa
+            acadêmica.
+          </Typography>
+          <Typography level="body-sm" sx={{ my: 1, mb: 3 }}>
+            Pedimos que avalie cada sentimento que você tem em relação à sua
+            graduação, atribuindo um número de 1 a 3, de acordo com a frequência
+            com que esses sentimentos emergem em seus pensamentos. Utilize a
+            seguinte escala:
+          </Typography>
+          <Typography level="body-sm" sx={{ my: 1, mb: 3 }}>
+            1 - NUNCA ou RARAMENTE 2 - FREQUENTEMENTE 3 - SEMPRE
+          </Typography>
+        </div>
+        <Formik
+          initialValues={formValues}
+          validationSchema={validationSchema}
+          onSubmit={handleFormSubmit}
         >
-          <Box
-            component="main"
-            sx={{
-              my: "auto",
-              py: 2,
-              pb: 5,
-              display: "flex",
-              flexDirection: "column",
-              gap: 2,
-              width: 600,
-              maxWidth: "100%",
-              mx: "auto",
-              borderRadius: "sm",
-              "& form": {
-                display: "flex",
-                flexDirection: "column",
-                gap: 2,
-              },
-            }}
-          >
-            <div>
-              <Typography component="h1" fontSize="xl2" fontWeight="lg">
-                Prezado(a) Estudante
-              </Typography>
-              <Typography level="body-sm" sx={{ my: 1, mb: 3 }}>
-                É com grande interesse que convidamos você a participar de um
-                momento fundamental para a pesquisa que está sendo conduzida no
-                âmbito da UniRedentor. Esta pesquisa tem como objetivo
-                aprofundar nossa compreensão sobre as percepções e sentimentos
-                dos estudantes em relação à sua experiência acadêmica,
-                especificamente no que diz respeito à graduação que estão
-                cursando.
-              </Typography>
-              <Typography level="body-sm" sx={{ my: 1, mb: 3 }}>
-                Sabemos que a vida acadêmica é repleta de desafios e
-                realizações, e a sua perspectiva é inestimável para enriquecer
-                nosso entendimento sobre os diversos aspectos envolvidos. Sua
-                participação ao responder o questionário contribuirá
-                significativamente para o avanço do conhecimento na área de
-                Ensino das Ciências e Saúde, possibilitando a criação de
-                estratégias mais eficazes para melhorar a qualidade da educação
-                e o bem-estar dos estudantes.
-              </Typography>
-              <Typography level="body-sm" sx={{ my: 1, mb: 3 }}>
-                Este SCORE busca capturar seus sentimentos e percepções genuínos
-                em relação à sua graduação. Queremos que você se sinta à vontade
-                para expressar seus pensamentos de maneira verdadeira e
-                autêntica. Suas respostas serão tratadas com total
-                confidencialidade e serão utilizadas apenas para fins de análise
-                estatística e pesquisa acadêmica.
-              </Typography>
-              <Typography level="body-sm" sx={{ my: 1, mb: 3 }}>
-                Pedimos que avalie cada sentimento que você tem em relação à sua
-                graduação, atribuindo um número de 1 a 3, de acordo com a
-                frequência com que esses sentimentos emergem em seus
-                pensamentos. Utilize a seguinte escala:
-              </Typography>
-              <Typography level="body-sm" sx={{ my: 1, mb: 3 }}>
-                1 - NUNCA ou RARAMENTE 2 - FREQUENTEMENTE 3 - SEMPRE
-              </Typography>
-            </div>
-            <Formik
-              initialValues={formValues}
-              validationSchema={validationSchema}
-              onSubmit={handleFormSubmit}
-            >
-              {({
-                touched,
-                values,
-                errors,
-                handleChange,
-                handleSubmit,
-                setFieldValue,
-                isSubmitting,
-              }) => {
-                return (
-                  <form onSubmit={handleSubmit}>
-                    <Input
-                      type="hidden"
-                      name="userEmail"
-                      value={values.userEmail}
-                    />
-                    <Input type="hidden" name="image" value={values.image} />
+          {({
+            touched,
+            values,
+            errors,
+            handleChange,
+            handleSubmit,
+            isSubmitting,
+          }) => {
+            return (
+              <form onSubmit={handleSubmit}>
+                <Input
+                  type="hidden"
+                  name="userEmail"
+                  value={values.userEmail}
+                />
 
-                    <Container
-                      maxWidth="md"
-                      style={{ marginBottom: theme.spacing(3) }}
+                <Container
+                  maxWidth="md"
+                  style={{ marginBottom: theme.spacing(3) }}
+                >
+                  <Box
+                    style={{
+                      padding: theme.spacing(3),
+                    }}
+                  >
+                    <FormControl
+                      error={errors.resposta1 && touched.resposta1}
+                      fullWidth
                     >
-                      <Box
-                        style={{
-                          padding: theme.spacing(3),
-                        }}
+                      <FormLabel>
+                        Em geral, sinto-me motivado com meu curso:
+                      </FormLabel>
+                      <Select
+                        name="resposta1"
+                        value={values.resposta1}
+                        onChange={handleChange}
+                        label="Selecione uma opção"
+                        fullWidth
                       >
-                        <FormControl
-                          error={errors.resposta1 && touched.resposta1}
-                          fullWidth
-                        >
-                          <FormLabel>Em geral, sinto-me motivado com meu curso:</FormLabel>
-                          <Select
-                            name="resposta1"
-                            value={values.resposta1}
-                            onChange={handleChange}
-                            label="Selecione uma opção"
-                            fullWidth
-                          >
-                            <MenuItem value={"1 - NUNCA ou RARAMENTE"}>
-                              1 - NUNCA ou RARAMENTE
-                            </MenuItem>
-                            <MenuItem value={"2 - FREQUENTEMENTE"}>
-                              2 - FREQUENTEMENTE
-                            </MenuItem>
-                            <MenuItem value={"3 - SEMPRE"}>3 - SEMPRE</MenuItem>
-                          </Select>
-                          <FormHelperText>
-                            {errors.resposta1 && touched.resposta1
-                              ? errors.resposta1
-                              : null}
-                          </FormHelperText>
-                        </FormControl>
-                      </Box>
-                    </Container>
+                        <MenuItem value={"1 - NUNCA ou RARAMENTE"}>
+                          1 - NUNCA ou RARAMENTE
+                        </MenuItem>
+                        <MenuItem value={"2 - FREQUENTEMENTE"}>
+                          2 - FREQUENTEMENTE
+                        </MenuItem>
+                        <MenuItem value={"3 - SEMPRE"}>3 - SEMPRE</MenuItem>
+                      </Select>
+                      <FormHelperText>
+                        {errors.resposta1 && touched.resposta1
+                          ? errors.resposta1
+                          : null}
+                      </FormHelperText>
+                    </FormControl>
+                  </Box>
+                </Container>
 
-                    <Container
-                      maxWidth="md"
-                      style={{ marginBottom: theme.spacing(3) }}
-                    >
-                      <Box textAlign="right">
-                        {isSubmitting ? (
-                          <CircularProgress
-                            sx={{ display: "block", margin: "10px auto" }}
-                          />
-                        ) : (
-                          <Button
-                            fullWidth
-                            type="submit"
-                            variant="contained"
-                            color="primary"
-                          >
-                            Enviar Respostas!
-                          </Button>
-                        )}
-                      </Box>
-                    </Container>
-                  </form>
-                );
-              }}
-            </Formik>
-          </Box>
-          <Box component="footer" sx={{ py: 3 }}>
-            <Typography level="body-xs" textAlign="center">
-              © UniRedentor {new Date().getFullYear()}
-            </Typography>
-          </Box>
+                <Container
+                  maxWidth="md"
+                  style={{ marginBottom: theme.spacing(3) }}
+                >
+                  <Box textAlign="right">
+                    {isSubmitting ? (
+                      <CircularProgress
+                        sx={{ display: "block", margin: "10px auto" }}
+                      />
+                    ) : (
+                      <Button
+                        fullWidth
+                        type="submit"
+                        variant="contained"
+                        color="primary"
+                      >
+                        Enviar Respostas!
+                      </Button>
+                    )}
+                  </Box>
+                </Container>
+              </form>
+            );
+          }}
+        </Formik>
+        <Box component="footer" sx={{ py: 3 }}>
+          <Typography level="body-xs" textAlign="center">
+            © UniRedentor {new Date().getFullYear()}
+          </Typography>
         </Box>
-      </Box>
+      </Container>
     </TemplateDefault>
   );
 };
@@ -262,15 +211,13 @@ export async function getServerSideProps({ req }) {
   let token = session?.accessToken || session?.user?.email || "";
 
   const user = await UsersModel.find({
-    "user.email": session.user.email,
+    email: session?.user?.email,
   });
 
-  console.log(session);
-  console.log(token);
+  console.log(user);
 
   return {
     props: {
-      session: JSON.parse(JSON.stringify(session)),
       user: JSON.parse(JSON.stringify(user)),
     },
   };
