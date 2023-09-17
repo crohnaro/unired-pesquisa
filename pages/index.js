f4022a11e2cbe13efce67e3cb526f3ae3b9e496d
crohnaro <vinicioscararine95@gmail.com>
2023-09-17 18:55:41 -0300

    refactor: :recycle: removing unnecessary imports

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,5 @@
 import * as React from "react";
 
-import DarkModeRoundedIcon from "@mui/icons-material/DarkModeRounded";
-import LightModeRoundedIcon from "@mui/icons-material/LightModeRounded";
-
 import { useRouter } from "next/router";
 
 import axios from "axios";
