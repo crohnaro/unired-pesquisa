4a0deb6d9ffb9f8fd211c87c80cff46e44f6d715
crohnaro <vinicioscararine95@gmail.com>
2023-09-18 11:45:48 -0300

    fix: :bug: fixing import

--- pages/auth/signup/index.js
+++ pages/auth/signup/index.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-import { CssVarsProvider } from "@mui/joy/styles";
+import { CssVarsProvider, useColorScheme } from "@mui/joy/styles";
 import GlobalStyles from "@mui/joy/GlobalStyles";
 import CssBaseline from "@mui/joy/CssBaseline";
 import PropTypes from "prop-types";
